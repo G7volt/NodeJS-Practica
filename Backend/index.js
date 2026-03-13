3df8dd5dd46424bc4bd9445d08d2cb8c6a38cb27
Christopher Sanchez <diegsanchez27@gmail.com>
2026-03-13 11:23:44 -0600

    archivos de seguridad añadidos

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 import app from "./app.js";
 import "./database.js"
+import { config } from "./SRC/src/config.js";
 
 //creamos la funcion 
 //que se encarga de ejecutar el servidor
 
 async function main() {
-    app.listen(4000);
+    app.listen(config.server.port);
+    console.log("Servidor escuchando en el puerto " + config.server.port);
 }
 
     main();
